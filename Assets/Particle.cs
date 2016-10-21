e6e93ade1eb3ab1610bed6e58257fc17d37ca638
contoracutar <contoracutar@softbank.ne.jp>
2016-10-22 02:07:56 +0900

    フェード調整

--- Assets/Particle.cs
+++ Assets/Particle.cs
@@ -7,6 +7,7 @@ public class Particle : MonoBehaviour {
 
 	Vector3 velocity = Vector3.zero;
 
+	Color alpha0 = new Color (1, 1, 1, 0);
 
 	public void Init (Vector3 vel) {
 
@@ -16,6 +17,8 @@ public class Particle : MonoBehaviour {
 	void Start () {
 		
 		GetComponent<SpriteRenderer> ().color = new Color (Random.value, Random.value, Random.value, 1);
+		alpha0 = GetComponent<SpriteRenderer> ().color;
+		alpha0.a = 0;
 
 		rev = Random.Range (-1.0f, 1.0f);
 
@@ -27,6 +30,9 @@ public class Particle : MonoBehaviour {
 		velocity *= 0.95f;
 		transform.position += velocity;
 		transform.rotation = Quaternion.AngleAxis (rev * 50, Vector3.forward) * transform.rotation;
-		transform.localScale = Vector3.Lerp (transform.localScale, Vector3.zero, 0.05f);
+//		transform.localScale = Vector3.Lerp (transform.localScale, Vector3.zero, 0.05f);
+
+		SpriteRenderer sr = GetComponent<SpriteRenderer> ();
+		sr.color = Color.Lerp (sr.color, alpha0, 0.05f);
 	}
 }
